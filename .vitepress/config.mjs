d926d3df9674ddfd25007700ee3f4189576aed23
haoguosheng <haoguoshengkf@126.com>
2024-09-13 17:29:11 +0800

    进一步完善

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,14 +2,22 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: 'zh-CN',
+  cleanUrls:true, //开启纯净链接
   base: "/vitepress-doc/",
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  title: "guothion 个人文档",
+  description: "guothion的个人文档库",
+  appearance:true, 
+  ignoreDeadLinks: false,
+  head: [
+    ['link', { rel: 'icon', href: './public/favicon.ico' }]
+  ],
   themeConfig: {
+    logo: './public/logo.png',
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '主页', link: '/' },
+      { text: '快速上手', link: '/markdown-examples' }
     ],
 
     sidebar: [
@@ -23,7 +31,28 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/guothion/vitepress-doc' }
+    ],
+    search: { 
+      provider: 'local'
+    }, 
+    footer: {
+      message: 'person example page'
+    },
+    // sidebar: [
+    //   {
+    //     //分组标题
+    //     text: '指南',
+    //     items: [
+    //       { text: '前言', link: '/' },
+    //       { text: '快速上手', link: '/' },
+    //       { text: '配置', link: '/' },
+    //       { text: '页面', link: '/' },
+    //       { text: 'Frontmatter', link: '/' },
+    //       { text: 'Markdown', link: '/' },
+    //       { text: '静态部署', link: '/' },
+    //     ],
+    //   },
+    // ],
   }
 })
--- index.md
+++ index.md
@@ -3,23 +3,23 @@
 layout: home
 
 hero:
-  name: "My Awesome Project"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  name: "学习文档"
+  text: "前端学习笔记"
+  tagline: guothion的全栈路
   actions:
     - theme: brand
-      text: Markdown Examples
+      text: 个人介绍
       link: /markdown-examples
     - theme: alt
-      text: API Examples
+      text: 学习规划
       link: /api-examples
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Canvas从入门到实战
+    details: 从 0 开始学习Canvas到精通图形学
+  - title: 数据结构和算法
+    details: 基本算法介绍以及例题讲解
+  - title: Node 学习
+    details: 作为一个全栈工程师，熟练使用Node还是很重要的
 ---
 
